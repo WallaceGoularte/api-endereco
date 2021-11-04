9cd4fc2da1860193ee421d70d69882889d99d7bf
Wallace Oliveira <wallace.oliveira@temasistemas.com.br>
2021-11-04 01:10:40 -0300

    Ajustes endpoint

--- src/main/java/com/api/consulta/cep/resources/EnderecoResource.java
+++ src/main/java/com/api/consulta/cep/resources/EnderecoResource.java
@@ -1,6 +1,9 @@
 package com.api.consulta.cep.resources;
 
+import java.text.MessageFormat;
 import java.util.List;
+
+import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.api.consulta.cep.domain.Endereco;
 import com.api.consulta.cep.dto.EnderecoDTO;
@@ -28,12 +32,38 @@ public class EnderecoResource {
 	
 	@RequestMapping(value = "/{cep}", method = RequestMethod.GET)
 	public ResponseEntity<Endereco> listarJogosPorId(@PathVariable final String cep) {
-		return ResponseEntity.ok().body(this.enderecoService.buscarEnderecoPorCep(cep));
+		Endereco endereco = this.enderecoService.buscarEnderecoPorCep(cep);
+		
+		if (endereco.equals(null)) {
+			RestTemplate restTemplate = new RestTemplate();
+
+			return ResponseEntity.ok().body(restTemplate.getForObject(this.getUrlApiCep(cep),	Endereco.class));
+		}
+		
+		
+		return ResponseEntity.ok().body(endereco);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public Endereco inserir(@RequestBody Endereco endereco) {
 		return this.enderecoService.inserirEndereco(endereco);
+	}
+	
+	@RequestMapping(value = "/{cep}", method = RequestMethod.POST)
+	public Endereco inserir(@RequestBody EnderecoDTO dto) {
+		if (!dto.getCep().equals(null) && !dto.getCep().isEmpty()) {
+			RestTemplate restTemplate = new RestTemplate();
+			
+			final Endereco endereco = restTemplate.getForObject(this.getUrlApiCep(dto.getCep()), Endereco.class);
+			return this.enderecoService.inserirEndereco(endereco);
+		} else {
+			throw new RuntimeException("Cep inválido");
+		}
+		
+	}
+
+	private String getUrlApiCep(String cep) {
+		return MessageFormat.format("https://viacep.com.br/ws/{0}/json/", cep);
 	}
 
 }
